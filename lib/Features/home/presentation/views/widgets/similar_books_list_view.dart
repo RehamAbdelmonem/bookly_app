59ea9b80255ebf531872820b0e700bbbd11c7412
RehamAbdelmonem <rehamabdelmonem79@gmail.com>
2023-12-20 03:16:04 +0200

    book details view done

--- lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,19 +1,19 @@
-
-import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
-class FeatureBooksListView extends StatelessWidget {
-  const FeatureBooksListView({super.key});
+import 'custom_book_item.dart';
+
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
+      height: MediaQuery.of(context).size.height * .15,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemBuilder: (context,index){
         return const Padding(
-          padding: EdgeInsets.symmetric(horizontal:8.0),
+          padding: EdgeInsets.symmetric(horizontal:5),
           child: CustomBookImage(),
         );
       }),
