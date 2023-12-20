4edefc9a877b21eab75551738c00d54d0d4ad975
RehamAbdelmonem <rehamabdelmonem79@gmail.com>
2023-12-20 02:03:46 +0200

    home view done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:bookly_app/Features/Splash/presentation/views/splash_view.dart';
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -12,10 +12,14 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
       debugShowCheckedModeBanner: false,
-      home: const SplashView(),
     );
   }
 }
+
